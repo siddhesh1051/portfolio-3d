d995c5bca3bd5c42b85075dd053bd2cdec6133a0
siddhesh1051 <siddhesh1052001@gmail.com>
2023-07-02 20:53:48 +0530

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
-
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import ReactGA from "react-ga4";
+
+
 
 const App = () => {
+
+  const TRACKING_ID = 'G-VNF5JE3HEJ';
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.send({ hitType: "pageview", page: "/", title: "Home View" });
+
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
