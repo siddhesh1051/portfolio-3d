7ebfb0aaa146db1d6c978c9087b4a26e7f48e3bf
siddhesh1051 <siddhesh1052001@gmail.com>
2024-05-05 18:31:49 +0530

    Stars bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 import ReactGA from "react-ga4";
-
-
+import StarCanvas from "./components/canvas/StarCanvas";
 
 const App = () => {
-
-  const TRACKING_ID = 'G-VNF5JE3HEJ';
+  const TRACKING_ID = "G-VNF5JE3HEJ";
   ReactGA.initialize(TRACKING_ID);
   ReactGA.send({ hitType: "pageview", page: "/", title: "Home View" });
 
-
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <StarCanvas />
+
+        <div className=" bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
@@ -23,13 +32,12 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
-          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { BsCloudDownload } from './../../node_modules/react-icons/bs/index.esm';
+import { BsCloudDownload } from "./../../node_modules/react-icons/bs/index.esm";
 
 const Hero = () => {
   return (
@@ -9,38 +8,37 @@ const Hero = () => {
       <div
         className={`absolute inset-0 top-[200px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi 👋, I'm <span className='text-[#915EFF]'>Siddhesh Patil</span>
+            Hi 👋, I'm <span className="text-[#915EFF]">Siddhesh Patil</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a MERN Stack Developer and <br className='sm:block hidden' />
+            I am a MERN Stack Developer and <br className="sm:block hidden" />
             Passionate about learning new Technologies
           </p>
-          <a href="https://drive.google.com/file/d/1M2RYJB-tVrjytPhNcuBgrpgF1ov0tx6H/view?usp=sharing" target="_blank" rel="noreferrer">
-          <button class="transition duration-500 border-0 text-lg h-12 w-[10rem] bg-[#915EFF] hover:bg-[#543794] text-white mt-10 px-3 rounded-md">
-        
-          
-
-       <span>Resume </span>
-       <BsCloudDownload className="inline-block" />
-       
-       
-       </button>
-       </a>
+          <a
+            href="https://drive.google.com/file/d/1M2RYJB-tVrjytPhNcuBgrpgF1ov0tx6H/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button class="transition duration-500 border-0 text-lg h-12 w-[10rem] bg-[#915EFF] hover:bg-[#543794] text-white mt-10 px-3 rounded-md">
+              <span>Resume </span>
+              <BsCloudDownload className="inline-block" />
+            </button>
+          </a>
         </div>
       </div>
 
       {/* <ComputersCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full lg:flex justify-center items-center hidden'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full lg:flex justify-center items-center hidden">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -50,7 +48,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
